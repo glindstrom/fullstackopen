fb56e70760d135f541aef52b95bd2ec5edebc53d
Gabriel Lindström <gabriel.lindstrom@gmail.com>
2019-10-27 09:21:36 +0200

    Exercises 2.19-2.20 done

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react'
 import Persons from './components/Persons'
 import Filter from './components/Filter'
 import PersonsForm from './components/PersonsForm'
+import Notification from './components/Notification'
 import personService from './services/persons'
 
 const App = () => {
@@ -9,6 +10,21 @@ const App = () => {
     const [newName, setNewName] = useState('')
     const [newNumber, setNewNumber] = useState('')
     const [filter, setFilter] = useState('')
+    const [message, setMessage] = useState(null)
+    const [errorMessage, setErrorMessage] = useState(null)
+
+    const resetForm = () => {
+        setNewName('')
+        setNewNumber('')
+    }
+
+    const clearMessages = () => {
+        setTimeout(() => {
+            console.log('Clearing messages...')
+            setErrorMessage(null)
+            setMessage(null)
+        }, 5000)
+    }
 
     useEffect(() => {
         personService.getAll().then(persons => setPersons(persons))
@@ -23,19 +39,40 @@ const App = () => {
 
         const existingPersons = persons.filter(x => x.name === newName)
         if (existingPersons.length > 0) {
-            const result = window.confirm(`${newName} is already added to phonebook, replace the old number with a new one?`)
+            const result = window.confirm(
+                `${newName} is already added to phonebook, replace the old number with a new one?`
+            )
             if (result) {
                 person.id = existingPersons[0].id
-                personService.update(person.id, person).then(returnedPerson => {
-                    setPersons(persons.map(person => person.id !== returnedPerson.id ? person : returnedPerson))    
-                })
+                personService
+                    .update(person.id, person)
+                    .then(returnedPerson => {
+                        setPersons(
+                            persons.map(person =>
+                                person.id !== returnedPerson.id
+                                    ? person
+                                    : returnedPerson
+                            )
+                        )
+                        resetForm()
+                        setMessage(`Updated ${person.name}`)
+                        clearMessages()
+                    })
+                    .catch(error => {
+                        setErrorMessage(
+                            `Information of ${person.name} has already been removed from server`
+                        )
+                        clearMessages()
+                    })
             }
             return
         }
 
         personService.create(person).then(person => {
             setPersons(persons.concat(person))
-            setNewName('')
+            setMessage(`Added ${person.name}`)
+            clearMessages()
+            resetForm()
         })
     }
 
@@ -48,7 +85,17 @@ const App = () => {
         if (result) {
             personService
                 .remove(person.id)
-                .then(() => setPersons(persons.filter(x => x.id !== person.id)))
+                .then(() => {
+                    setPersons(persons.filter(x => x.id !== person.id))
+                    setMessage(`Deleted ${person.name}`)
+                    clearMessages()
+                })
+                .catch(error => {
+                    setErrorMessage(
+                        `Information of ${person.name} has already been removed from server`
+                    )
+                    clearMessages()
+                })
         }
     }
 
@@ -60,6 +107,8 @@ const App = () => {
 
     return (
         <div>
+            <Notification message={message} messageType="success" />
+            <Notification message={errorMessage} messageType="error" />
             <h2>Phonebook</h2>
             <Filter onChange={handleFilterChange} />
             <h3>Add a new</h3>
