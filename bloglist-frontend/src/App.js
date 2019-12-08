5961b33ec7a0331c3b8fbcb64349a47e823d7b7f
Gabriel Lindström <gabriel.lindstrom@gmail.com>
2019-12-08 15:06:10 +0200

    Exercise 5.2 done

--- bloglist-frontend/src/App.js
+++ bloglist-frontend/src/App.js
@@ -15,7 +15,15 @@ const App = () => {
     blogService.getAll().then(initialBlogs => {
       setBlogs(initialBlogs)
     })
-  })
+  }, [])
+
+  useEffect(() => {
+    const loggedUserJson = window.localStorage.getItem('loggedBlogAppUser')
+    if (loggedUserJson) {
+      const user = JSON.parse(loggedUserJson)
+      setUser(user)
+    }
+  }, [])
 
   const handleLogin = async event => {
     event.preventDefault()
@@ -24,6 +32,8 @@ const App = () => {
         username,
         password
       })
+
+      window.localStorage.setItem('loggedBlogAppUser', JSON.stringify(user))
       setUser(user)
       setUsername('')
       setPassword('')
@@ -33,6 +43,11 @@ const App = () => {
         setErrorMessage(null)
       }, 5000)
     }
+  }
+
+  const logout = () => {
+    window.localStorage.clear()
+    window.location.reload()
   }
 
   if (user === null) {
@@ -68,7 +83,9 @@ const App = () => {
   return (
     <div>
       <h2>blogs</h2>
-      <p>{user.name} logged in</p>
+      <p>
+        {user.name} logged in <button onClick={logout}>logout</button>
+      </p>
       {blogs.map(blog => (
         <Blog key={blog.id} blog={blog} />
       ))}
