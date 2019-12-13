c050f0a38279e22711fcd3cf7b73e5c1438256cc
Gabriel Lindström <gabriel.lindstrom@gmail.com>
2019-12-13 22:13:17 +0200

    Exercise 5.14 done

--- bloglist-frontend/src/components/SimpleBlog.test.js
+++ bloglist-frontend/src/components/SimpleBlog.test.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { render, cleanup } from '@testing-library/react'
+import { render, fireEvent } from '@testing-library/react'
 import SimpleBlog from './SimpleBlog'
 
 test('renders all fields', () => {
@@ -14,4 +14,22 @@ test('renders all fields', () => {
   expect(component.container).toHaveTextContent(blog.title)
   expect(component.container).toHaveTextContent(blog.author)
   expect(component.container).toHaveTextContent(`blog has ${blog.likes} likes`)
+})
+
+test('clicking like twice calls even handler twice', async () => {
+  const blog = {
+    title: 'blog title',
+    author: 'blog author',
+    likes: 0
+  }
+
+  const mockHandler = jest.fn()
+
+  const { getByText } = render(<SimpleBlog blog={blog} onClick={mockHandler} />)
+
+  const button = getByText('like')
+  fireEvent.click(button)
+  fireEvent.click(button)
+
+  expect(mockHandler.mock.calls.length).toBe(2)
 })
